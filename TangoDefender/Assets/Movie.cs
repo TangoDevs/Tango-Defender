aa51aa32c738b2027f6519e760fbc5104ad274a2
mysterydeveloper <manus.duggan@yahoo.com>
2016-03-30 12:23:07 +0100

    Scene set up

---
+++ TangoDefender/Assets/Movie.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Movie : MonoBehaviour {
+
+     // Use this for initialization
+     void Start () {
+		if(System.IO.File.Exists("Assets/entry.mp4"))
+      		 Debug.LogWarning("File exists");
+		else
+   			 Debug.LogWarning("File Not found");
+		playVideo ();
+	}
+
+	void playVideo(){
+
+		if (Handheld.PlayFullScreenMovie ("Assets/entry.mp4", Color.green, FullScreenMovieControlMode.Full, FullScreenMovieScalingMode.AspectFit)) {
+
+			Debug.Log ("hello");
+		};
+	}
+
+}
+
---
+++ TangoDefender/Assets/Scripts/Unit.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+	GameObject tards;
+	Transform target;
+	public float speed ;
+	Vector3[] path;
+	int targetIndex;
+    bool recheck=false;
+    int i=0;
+    bool changer=false;
+	GameObject plane,astar;
+
+	Grid g;
+
+
+    void Start()
+    {
+		plane=GameObject.Find("Plane");
+		astar=GameObject.Find("A*");
+		g=astar.GetComponent<Grid>();
+		tards = GameObject.Find ("Castle");
+		target = tards.transform;
+        PathRequestManager.RequestPath(transform.position, target.transform.position, OnPathFound);
+    }
+    void Update()
+    {
+       
+    }
+		
+    public void OnPathFound(Vector3[] newPath, bool pathSuccessful) {
+		if (pathSuccessful) {
+			path = newPath;
+			StopCoroutine("FollowPath");
+			StartCoroutine("FollowPath");
+		}
+	}
+
+	IEnumerator FollowPath() {
+		Vector3 currentWaypoint = path[0];
+
+		while (true) {
+           
+            if (transform.position == currentWaypoint) {
+				targetIndex ++;
+                
+                if (targetIndex >= path.Length) {
+                   
+					yield break;
+				}
+				currentWaypoint = path[targetIndex];
+
+			}
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+
+            yield return null;
+			}
+		}
+
+	void OnTriggerEnter(Collider col){
+		
+	}
+}
+
